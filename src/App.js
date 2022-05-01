bb14aed130b0bdf006fcc324d839c51e3ef04092
Virenk21 <viren.d222111@gmail.com>
2022-05-02 05:19:14 +0530

    added set headers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Title from "./components/Title";
-import Setup from './examples/1-first-request';
+import Setup from './examples/2-headers';
   
 function App () {
 return (
     <main>
       <Title />
-      
+
       <Setup/>
 
     </main>
